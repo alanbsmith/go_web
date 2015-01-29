8e04a9725272a3453e175a76c6067d39f1efb6c9
Alan Smith <a.bax.smith@gmail.com>
2015-01-28 17:52:00 -0700

    uses pg database in templates and adds a little love

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-  "fmt"
   "github.com/codegangsta/martini"
   "github.com/martini-contrib/render"
   "database/sql"
   "net/http"
   _ "github.com/lib/pq"
 )
+
+type Idea struct {
+  Title       string
+  Description string
+}
 
 func SetupDB() *sql.DB {
   db, err := sql.Open("postgres", "dbname=ideabox sslmode=disable")
@@ -29,23 +33,23 @@ func main() {
     Layout: "layout",
   }))
 
-  m.Get("/", func(rw http.ResponseWriter, r *http.Request, db *sql.DB) {
+  m.Get("/", func(ren render.Render, r *http.Request) {
+    ren.HTML(200,"landing_page", nil)
+  })
+
+  m.Get("/ideas", func(ren render.Render, r *http.Request, db *sql.DB) {
     rows, err := db.Query("SELECT title, description FROM ideas")
     PanicIf(err)
     defer rows.Close()
 
-    var title, description string
+    ideas := []Idea{}
     for rows.Next() {
-      err := rows.Scan(&title, &description)
+      i := Idea{}
+      err := rows.Scan(&i.Title, &i.Description)
       PanicIf(err)
-      fmt.Fprintf(rw, "Title: %s\nDescription: %s\n\n",title, description)
+      ideas = append(ideas, i)
     }
-    // ren.HTML(200,"landing_page", nil)
-  })
-
-  m.Get("/ideas", func(ren render.Render, r *http.Request) {
-  
-  ren.HTML(200,"ideas/index", nil)
+    ren.HTML(200,"ideas/index", ideas)
   })
 
   m.Run()
